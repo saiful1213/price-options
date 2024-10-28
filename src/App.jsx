d2fd49ee661196bf8fbd8adc2078482f936cda48
saiful islam <saifulislamptcbd@gmail.com>
2024-10-27 19:34:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-red-600 uppercase">hellow</h1>
+      <Button variant="destructive" size="lg">Click Me</Button>
+    </>
+  )
+}
+
+export default App
